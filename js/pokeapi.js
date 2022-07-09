d61fba21a1289c958174baed0d9dd28297351b89
Saregom <santi26reyes@gmail.com>
2022-07-09 17:16:45 -0500

    Correccion de sintaxis

--- js/pokeapi.js
+++ js/pokeapi.js
@@ -2,33 +2,31 @@ const query = function (selector) {
     return document.querySelector(selector)
 }
 
-document.form1.onsubmit = async function(e){
+document.form1.onsubmit = (e) => {
     e.preventDefault()
 
     const nameId = document.querySelector("input").value
 
-    await fetch(`https://pokeapi.co/api/v2/pokemon/${nameId}`)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${nameId}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
         query("#name").innerHTML = data.name
-        query("#id").innerHTML = "#" + data.id
+        query("#id").innerHTML = `#${data.id}`
         
         if(data.sprites.front_default === null){
-            query("#pokemon").src = "https://svgsilh.com/svg/1574006.svg"
+            query("#pokemon").src = "https://arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/LUOOHUM2OVEEXG7ZTRSNI6XWLY.png"
             query("#pokemon").style.padding = "20px"
         }else{
             query("#pokemon").src = data.sprites.front_default
             query("#pokemon").style.padding = "0"
         }
 
-        const abilities = data.abilities
         query("#abilities").innerHTML = ""
-        for(const ability of abilities){
-            query("#abilities").innerHTML += "<p class='ability'>" + ability.ability.name + "</p>"
+        for(const ability of data.abilities){
+            query("#abilities").innerHTML += `<p class="ability">${ability.ability.name}</p>` 
         }
     })
-    .catch(e => {
+    .catch(error => {
         alert("Pokemon not found")
         query("#name").innerHTML = "Pokemon not found"
         query("#id").innerHTML = "#0"
